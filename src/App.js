b2db30d4fe90e3b7116f33050d21c76869e0d0b1
behzad <behzad.b.i.g@gmail.com>
2022-12-05 11:56:00 +0330

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     
-      </header>
+        
+        <ChatEngine 
+          height="100vh" 
+          projectID="0cf7602b-2c08-4d6c-adf6-c13c50c568db"
+          userName={localStorage.getItem('username')}
+          userSecret={localStorage.getItem('password')}
+          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+          onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+        />
     </div>
   );
 }
